8eca3bba6e60d138887f98033a84e271e1899b6a
Ubpa <641614112@qq.com>
2020-01-29 19:29:17 +0800

    [CppPratices] add codes

---
+++ CppPratices/codes/src/exe/4_list_Polynomial/main.cpp
@@ -0,0 +1,25 @@
+#include "PolynomialList.h"
+#include <list>
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    PolynomialList p1("../data/P3.txt");
+	PolynomialList p2("../data/P4.txt");
+    PolynomialList p3;
+	p1.Print();
+	p2.Print();
+
+	p3=p1+p2;
+	p3.Print();
+    p3=p1-p2;
+	p3.Print();
+
+	p3=p1*p2;
+	p3.Print();
+
+	return 0;
+}
