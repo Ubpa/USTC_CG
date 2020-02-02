88b8757ae563569373da92384b297067d16d8683
Ubpa <641614112@qq.com>
2020-02-02 22:17:39 +0800

    [CppPratices] normalize

--- Homeworks/0_CppPratices/project/src/executables/1_BasicDArray/DArray.cpp
+++ Homeworks/0_CppPratices/project/src/executables/1_BasicDArray/DArray.cpp
@@ -2,100 +2,86 @@
 #include "DArray.h"
 
 // default constructor
-DArray::DArray()
-{
+DArray::DArray() {
 	Init();
 }
 
 // set an array with default values
-DArray::DArray(int nSize, double dValue)
-{
+DArray::DArray(int nSize, double dValue) {
 	//TODO
 }
 
-DArray::DArray(const DArray& arr)
-{
+DArray::DArray(const DArray& arr) {
 	//TODO
 }
 
 // deconstructor
-DArray::~DArray()
-{
+DArray::~DArray() {
 	Free();
 }
 
 // display the elements of the array
-void DArray::Print()
-{
+void DArray::Print() const {
 	//TODO
 }
 
 // initilize the array
-void DArray::Init()
-{
+void DArray::Init() {
 	//TODO
 }
 
 // free the array
-void DArray::Free()
-{
+void DArray::Free() {
 	//TODO
 }
 
 // get the size of the array
-int DArray::GetSize()
-{
+int DArray::GetSize() const {
 	//TODO
 	return 0;
 }
 
 // set the size of the array
-void DArray::SetSize(int nSize)
-{
+void DArray::SetSize(int nSize) {
 	//TODO
 }
 
 // get an element at an index
-double DArray::GetAt(int nIndex)
-{
+const double& DArray::GetAt(int nIndex) const {
 	//TODO
-	return 0.;
+	static double ERROR; // you should delete this line
+	return ERROR;
 }
 
 // set the value of an element 
-void DArray::SetAt(int nIndex, double dValue)
-{
+void DArray::SetAt(int nIndex, double dValue) {
 	//TODO
 }
 
 // overload operator '[]'
-double DArray::operator[](int nIndex) const
-{
+const double& DArray::operator[](int nIndex) const {
 	//TODO
-	return 0.;
+	static double ERROR; // you should delete this line
+	return ERROR;
 }
 
 // add a new element at the end of the array
-void DArray::PushBack(double dValue)
-{
+void DArray::PushBack(double dValue) {
 	//TODO
 }
 
 // delete an element at some index
-void DArray::DeleteAt(int nIndex)
-{
+void DArray::DeleteAt(int nIndex) {
 	//TODO
 }
 
 // insert a new element at some index
-void DArray::InsertAt(int nIndex, double dValue)
-{
+void DArray::InsertAt(int nIndex, double dValue) {
 	//TODO
 }
 
 // overload operator '='
-DArray& DArray::operator = (const DArray& arr)
-{
+DArray& DArray::operator = (const DArray& arr) {
 	//TODO
 	return *this;
 }
