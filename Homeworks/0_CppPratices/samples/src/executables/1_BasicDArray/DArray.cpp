33c4d25bb51e9536c8d7119905f3c16dace2e174
Ubpa <641614112@qq.com>
2020-02-02 17:02:24 +0800

    [CppPratices] optimize samples/1_BasicDArray

--- Homeworks/0_CppPratices/samples/src/executables/1_BasicDArray/DArray.cpp
+++ Homeworks/0_CppPratices/samples/src/executables/1_BasicDArray/DArray.cpp
@@ -3,6 +3,8 @@
 
 #include "DArray.h"
 
+#include <assert.h>
+
 using namespace std;
 
 // default constructor
@@ -14,12 +16,6 @@ DArray::DArray() {
 DArray::DArray(int nSize, double dValue)
 	: m_pData(new double[nSize]), m_nSize(nSize)
 {
-	if (!m_pData) {
-		cout << "Can not alloc memory in DArray::DArray()!" << endl;
-		m_nSize = 0;
-		return;
-	}
-
 	for (int i = 0; i < nSize; i++)
 		m_pData[i] = dValue;
 }
@@ -27,12 +23,6 @@ DArray::DArray(int nSize, double dValue)
 DArray::DArray(const DArray& arr)
 	: m_pData(new double[arr.m_nSize]), m_nSize(arr.m_nSize)
 {
-	if (!m_pData) {
-		cout << "Can not alloc memory in DArray::DArray()!" << endl;
-		m_nSize = 0;
-		return;
-	}
-
 	for (int i = 0; i < m_nSize; i++)
 		m_pData[i] = arr.m_pData[i];
 }
@@ -64,26 +54,17 @@ void DArray::Free() {
 	m_nSize = 0;
 }
 
-// judge the validate of an index
-bool DArray::InvalidateIndex(int nIndex) const {
-	return nIndex < 0 || nIndex >= m_nSize;
-}
-
 // get the size of the array
 int DArray::GetSize() {
 	return m_nSize;
 }
 
 // set the size of the array
-bool DArray::SetSize(int nSize) {
+void DArray::SetSize(int nSize) {
 	if (m_nSize == nSize)
-		return true;
+		return;
 
 	double* pData = new double[nSize];
-	if (!pData) {
-		cout << "Can not alloc memory in SetSize()!" << endl;
-		return false;
-	}
 
 	int copyNum = nSize < m_nSize ? nSize : m_nSize;
 	for (int i = 0; i < copyNum; i++)
@@ -94,29 +75,18 @@ bool DArray::SetSize(int nSize) {
 	delete[] m_pData;
 	m_pData = pData;
 	m_nSize = nSize;
-
-	return true;
 }
 
 // get an element at an index
 double DArray::GetAt(int nIndex) {
-	if (InvalidateIndex(nIndex)) {
-		cout << "Index is invalide in GetAt()!" << endl;
-		return 0.;
-	}
-
+	assert(nIndex >= 0 && nIndex < m_nSize);
 	return m_pData[nIndex];
 }
 
 // set the value of an element 
-bool DArray::SetAt(int nIndex, double dValue) {
-	if (InvalidateIndex(nIndex)) {
-		cout << "Index is invalide in SetAt()!" << endl;
-		return false;
-	}
-
+void DArray::SetAt(int nIndex, double dValue) {
+	assert(nIndex >= 0 && nIndex < m_nSize);
 	m_pData[nIndex] = dValue;
-	return true;
 }
 
 // overload operator '[]'
@@ -125,12 +95,8 @@ double DArray::operator[](int nIndex) const {
 }
 
 // add a new element at the end of the array
-bool DArray::PushBack(double dValue) {
+void DArray::PushBack(double dValue) {
 	double* pTemp = new double[static_cast<size_t>(m_nSize) + 1];
-	if (pTemp == nullptr) {
-		cout << "Can not alloc memory in PushBack()!" << endl;
-		return false;
-	}
 
 	for (int i = 0; i < m_nSize; i++)
 		pTemp[i] = m_pData[i];
@@ -140,22 +106,13 @@ bool DArray::PushBack(double dValue) {
 	delete[] m_pData;
 	m_pData = pTemp;
 	m_nSize++;
-
-	return true;
 }
 
 // delete an element at some index
-bool DArray::DeleteAt(int nIndex) {
-	if (InvalidateIndex(nIndex)) {
-		cout << "Index is invalide in DeleteAt()!" << endl;
-		return false;
-	}
+void DArray::DeleteAt(int nIndex) {
+	assert(nIndex >= 0 && nIndex < m_nSize);
 
 	double* pTemp = new double[static_cast<size_t>(m_nSize) - 1];
-	if (!pTemp) {
-		cout << "Can not alloc memory in DeleteAt()!" << endl;
-		return false;
-	}
 
 	for (int i = 0; i < nIndex; i++)
 		pTemp[i] = m_pData[i];
@@ -166,23 +123,13 @@ bool DArray::DeleteAt(int nIndex) {
 	delete[] m_pData;
 	m_pData = pTemp;
 	m_nSize--;
-
-	return true;
 }
 
 // insert a new element at some index
-bool DArray::InsertAt(int nIndex, double dValue) {
-	if (InvalidateIndex(nIndex))
-	{
-		cout << "Index is invalide in InsertAt()!" << endl;
-		return false;
-	}
+void DArray::InsertAt(int nIndex, double dValue) {
+	assert(nIndex<0 && nIndex > m_nSize); // nIndex == m_nSize is legal
 	
-	double* pTemp = new double[m_nSize + 1];
-	if (!pTemp) {
-		cout << "Can not alloc memory in InsertAt()!" << endl;
-		return false;
-	}
+	double* pTemp = new double[static_cast<size_t>(m_nSize) + 1];
 
 	for (int i = 0; i < nIndex; i++)
 		pTemp[i] = m_pData[i];
@@ -195,8 +142,6 @@ bool DArray::InsertAt(int nIndex, double dValue) {
 	delete[] m_pData;
 	m_pData = pTemp;
 	m_nSize++;
-
-	return true;
 }
 
 // overload operator '='
--- Homeworks/0_CppPratices/samples/src/executables/1_BasicDArray/DArray.h
+++ Homeworks/0_CppPratices/samples/src/executables/1_BasicDArray/DArray.h
@@ -14,16 +14,16 @@ public:
 	void Print(); // print the elements of the array
 
 	int GetSize(); // get the size of the array
-	bool SetSize(int nSize); // set the size of the array
+	void SetSize(int nSize); // set the size of the array
 
 	double GetAt(int nIndex); // get an element at an index
-	bool SetAt(int nIndex, double dValue); // set the value of an element
+	void SetAt(int nIndex, double dValue); // set the value of an element
 
 	double operator[](int nIndex) const; // overload operator '[]'
 
-	bool PushBack(double dValue); // add a new element at the end of the array
-	bool DeleteAt(int nIndex); // delete an element at some index
-	bool InsertAt(int nIndex, double dValue); // insert a new element at some index
+	void PushBack(double dValue); // add a new element at the end of the array
+	void DeleteAt(int nIndex); // delete an element at some index
+	void InsertAt(int nIndex, double dValue); // insert a new element at some index
 
 	DArray& operator = (const DArray& arr); //overload operator '='
 
@@ -34,7 +34,6 @@ private:
 private:
 	void Init(); // initilize the array
 	void Free(); // free the array
-	bool InvalidateIndex(int nIndex) const; // judge the validate of an index
 };
 
 #endif // !_DYNAMICARRAY_H_
