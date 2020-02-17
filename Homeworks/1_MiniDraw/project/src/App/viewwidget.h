a3cd41bd5e600dc9ffb4103859021b01d2fb1b87
Ubpa <641614112@qq.com>
2020-02-18 01:01:14 +0800

    [MiniDraw] \r\n

--- Homeworks/1_MiniDraw/project/src/App/viewwidget.h
+++ Homeworks/1_MiniDraw/project/src/App/viewwidget.h
@@ -18,13 +18,33 @@ class ViewWidget : public QWidget
 	Q_OBJECT
 
 public:
-	ViewWidget(QWidget *parent = 0);
+	ViewWidget(QWidget* parent = 0);
 	~ViewWidget();
 
 private:
 	Ui::ViewWidget ui;
 
-private:	bool draw_status_;	QPoint start_point_;	QPoint end_point_;	Shape::Type type_;	Shape *shape_;	std::vector<Shape*> shape_list_;public:	void mousePressEvent(QMouseEvent *event);	void mouseMoveEvent(QMouseEvent *event);	void mouseReleaseEvent(QMouseEvent *event);public:	void paintEvent(QPaintEvent *);signals:	public slots :		void setLine();	void setRect();
+private:
+	bool draw_status_;
+	QPoint start_point_;
+	QPoint end_point_;
+	Shape::Type type_;
+	Shape* shape_;
+	std::vector<Shape*> shape_list_;
+
+
+public:
+	void mousePressEvent(QMouseEvent* event);
+	void mouseMoveEvent(QMouseEvent* event);
+	void mouseReleaseEvent(QMouseEvent* event);
+
+public:
+	void paintEvent(QPaintEvent*);
+signals:
+public slots:
+	void setLine();
+	void setRect();
+
 };
 
 #endif // VIEWWIDGET_H
