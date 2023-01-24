7542cad42326092100bbb599cb843d903348b14c
Rainzor <runze.wang02@outlook.com>
2023-01-24 16:41:07 +0800

    Complete Roche model in Taichi

--- Homeworks/7_SimulationTaichi/project/src/app/Poolball.cpp
+++ Homeworks/7_SimulationTaichi/project/src/app/Poolball.cpp
@@ -32,7 +32,7 @@ const real inv_dx = 1.0_f / dx;
 const auto particle_mass = 1.0_f;
 const auto vol = 1.0_f;        // Particle Volume
 const auto hardening = 10.0_f; // Snow hardening factor
-const auto E = 1e4_f;          // Young's Modulus
+const auto E = 2e5_f;          // Young's Modulus
 const auto nu = 0.2_f;         // Poisson ratio
 const bool plastic = true;
 
@@ -40,9 +40,7 @@ const bool plastic = true;
 const real mu_0 = E / (2 * (1 + nu));
 const real lambda_0 = E * nu / ((1+nu) * (1 - 2 * nu));
 
-enum Material { Water, Jelly,Snow, Metal};
-int color[] = {0xF2522E, 0x023059, 0x025940, 0x5400B4, 0x0D0D0D, 0x0061D3, 0x401D26, 0xCFD600, 0xE76C00, 0xC11321};
-const real G = 1e-3_f;
+enum Material { Water, Jelly,Snow};
 /***********************************(1)*****************************************/
 // the definition of a particle
 struct Particle {
@@ -78,43 +76,6 @@ struct Particle {
 };
 //std::vector<Particle> particles;
 
-struct Planet {
-  Vec center;
-  real radius;
-  real mass;
-  real rho;
-  real omega;
-  int color;
-  Material material;
-  Planet(Vec x, real r, real M,real rho, int c):
-    center(x),
-    radius(r),
-    mass(M),
-    rho(rho),
-    omega(0),
-    color(c),
-    material(Metal){}
-
-  Planet(Vec x, real r, real M, real rho,real o, int c, Material m) :
-      center(x),
-      radius(r),
-      mass(M),
-      rho(rho),
-      omega(o),
-      color(c),
-      material(m){}
-  Planet():
-    center(Vec(0)),
-    radius(0),
-    mass(0),
-    rho(0),
-    omega(0),
-    color(0),
-    material(Metal) {}
-
-};
-
-
 ////////////////////////////////////////////////////////////////////////////////
 
 // Vector3: [velocity_x, velocity_y, mass]
@@ -126,15 +87,12 @@ class mpm
 private:
   // Vector3: [velocity_x, velocity_y, mass]
   Vector3 grid[n+1][n+1];
-  std::vector<Vector2i> grid_indices;
+   
 public:
   std::vector<Particle> particles;
   Vector3 vertices[n_elements];
   std::vector<Vector3> obstacles;//[center_x, center_y, radius]
-  struct Planet center_planet;
-  struct Planet satellite;
-  std::vector<Vec> orbit;
- public: 
+public : 
   mpm(){};
   ~mpm(){};
 
@@ -146,11 +104,7 @@ public:
 
   void add_circle(Vec center, real radius,int num, int c, Vec v, Material material);
 
-  void add_satellite(Vec center, real radius, real rho, int c, Material material);
-
   void add_obstacle(Vec center, real radius);
-
-  void add_center_planet(Vec center, real radius, real rho, int c);
 
   void substep();
 
@@ -161,25 +115,30 @@ private:
   void grid_op();
   void G2P();
 
-  void Roche_model();
-
-  bool check_outrange(Vec p);
 };
 
 int main() {
   GUI gui("Real-time 2D MLS-MPM", window_size, window_size);
   auto &canvas = gui.get_canvas();
   mpm m;
-  Vec center(0.5, 0.5);
-  m.add_center_planet(center, 25, 10, 0xFFFFFF);
-  real satellite_rho = 1;
-  real satellite_radius = 10;
-  real Roche_radius = 2.5 * m.center_planet.radius * real(pow(m.center_planet.rho / satellite_rho, 1 / 3));
-
-  m.add_satellite(center + Vec(0, (Roche_radius+satellite_radius) / window_size) + Vec(0,0.3), satellite_radius, satellite_rho, 0xBDE4FF, Metal);
-  //m.add_circle(center, 20, 100, 0x023059, Vec(0,0), Snow);
-
-  //m.add_circle(center+Vec(0,0.2), 20, 1000, 0x023059, Vec(0, 0), Snow);
+  // m.add_square(Vec(0.55, 0.45), 0x87CEFA, Water);
+  // m.add_square(Vec(0.45, 0.65), 0xFFFAFA, Snow);
+  // m.add_square(Vec(0.55, 0.85), 0xED553B, Jelly);
+  // m.add_spring(Vec(0.3, 0.8), 0xFFFAFA, Jelly);
+  real r = 0.03;
+  Vec center(0.5, 0.4);
+  int color[] = {0xF2522E,0x023059,0x025940,0x5400B4,0x0D0D0D,0x0061D3,0x401D26,0xCFD600,0xE76C00,0xC11321};
+  for (int i = 0, k = 0; i < 4; i++){
+	  for (int j = 0; j <= i; j++) {
+		  m.add_circle(center + Vec(2*j-i, i*sqrt(3))*r, r-0.005, 1500, color[k], Vec(0), Jelly);
+          k++;
+   	  }
+  }
+  m.add_circle(Vec(0.5, 0.1), r - 0.005, 1500, 0xFFFAFA, Vec(0,200), Jelly);
+  m.add_obstacle(Vec(0.1, 0.1), 40);
+  m.add_obstacle(Vec(0.9, 0.1), 40);
+  m.add_obstacle(Vec(0.1, 0.9), 40);
+  m.add_obstacle(Vec(0.9, 0.9), 40);
   int frame = 0;
   // Main Loop
   for (int step = 0;; step++) {
@@ -189,9 +148,9 @@ int main() {
     // Visualize frame
     if (step % int(frame_dt / dt) == 0) {
       // Clear background
-      canvas.clear(0x0D0D0D);
+      canvas.clear(0x44803F);
       // Box
-      canvas.rect(Vec(0.01), Vec(0.99)).radius(2).color(0xD0ECF2).close();
+      canvas.rect(Vec(0.04), Vec(0.96)).radius(2).color(0xD0ECF2).close();
 
       //center collision circle
       for(auto& o : m.obstacles){
@@ -199,24 +158,14 @@ int main() {
       }
       // Particles
       for (auto& p : m.particles) {
-        canvas.circle(p.x).radius(0.5).color(p.c);
+        canvas.circle(p.x).radius(1).color(p.c);
       }
-      //canvas.circle(m.particles[0].x).radius(5).color(m.particles[0].c);
-      // Planet
-      if(m.center_planet.mass > 0){
-        canvas.circle(m.center_planet.center).radius(m.center_planet.radius).color(m.center_planet.color);
-      }
-      canvas.circle(m.satellite.center).radius(3).color(0xED553B);
-      for (auto& o : m.orbit) {
-        canvas.circle(o).radius(0.25).color(0xDBF227);
-      }
+
       // Update image  
       gui.update();
 
       // Write to disk (optional)
-      canvas.img.write_as_image(fmt::format("Roche_limit_balance/{:05d}.png", frame++));
-      if (frame > 6000)
-          return 0;
+      canvas.img.write_as_image(fmt::format("pool_ball/{:05d}.png", frame++));
     }
   }
 }
@@ -243,79 +192,25 @@ void mpm::add_spring(Vec center, int c, Material material) {
 }
 
 void mpm::add_circle(Vec center, real radius, int num, int c, Vec v, Material material) {
-  real r = radius / window_size;
 	for (int i = 0; i < num;) {
-        /*real d_theta = 2 * pi / num;
-        real theta = d_theta * i;
-        Vec pos = Vec(r * cos(theta), r * sin(theta));*/
-        auto pos = (Vec::rand() * 2.0_f - Vec(1)) * r;
-        if (pos.x * pos.x + pos.y * pos.y < r * r) {
+        auto pos = (Vec::rand() * 2.0_f - Vec(1)) * radius;
+        if (pos.x * pos.x + pos.y * pos.y < radius * radius) {
             particles.push_back(Particle(pos + center, c, v, material));
             i++;
         }
 	}
 }
 
-void mpm::add_satellite(Vec center, real radius, real rho, int c, Material material) {
-  if(center_planet.mass == 0){
-    std::cout << "No center center_planet" << std::endl;
-    exit(0);
-  }else{
-    real r = radius / window_size;
-    int n = int(rho *pow(radius,3));  // * 4.0f / 3.0f * pi
-    //int n = 0;
-    Vec cp_center = center_planet.center;
-    real cp_r = center_planet.radius / window_size;
-    Vec d = center - cp_center;
-    real dist = sqrt(d.x*d.x + d.y*d.y);
-    if(dist < cp_r + r){
-      std::cout << "Too close to center center_planet" << std::endl;
-      exit(0);
-    }
-    real omega = sqrt(G * center_planet.mass / (dist)) / dist;
-    Vec v = Vec(-d.y,   d.x) / dist * sqrt(G * center_planet.mass / dist);
-
-    //add center_particle
-    particles.push_back(Particle(center, c, v, material));
-
-    for (int i = 0; i < n;) {
-      /*real d_theta = 2 * pi / num;
-      real theta = d_theta * i;
-      Vec pos = Vec(r * cos(theta), r * sin(theta));*/
-      auto pos = (Vec::rand() * 2.0_f - Vec(1)) * r;
-      if (pos.x * pos.x + pos.y * pos.y <= r * r) {
-        d = pos + center - cp_center;
-        v = omega * Vec(-d.y, d.x);
-        //v = Vec(0);
-        particles.push_back(Particle(pos + center, c, v, material));
-        i++;
-      }
-    }
-    satellite = Planet(center, radius, n*particle_mass, rho,omega, c, material);
-  }
-  
-}
-
-
 void mpm::add_obstacle(Vec center, real radius) {
   obstacles.push_back(Vector3(center, radius));
 }
 
-void mpm::add_center_planet(Vec center, real radius, real rho, int c) {
-  // Randomly sample 1000 particles in the square
-  real mass = rho * pow(radius, 3);// * 4.0f / 3.0f * pi;
-  center_planet = Planet(center, radius, mass*particle_mass,rho, c);
-}
-
 void mpm::substep() {
-    if(satellite.material==Metal)
-        Roche_model();
-    else {
-        std::memset(grid, 0, sizeof(grid));
-        P2G();
-        grid_op();
-        G2P();
-    }
+  // Reset grid
+  std::memset(grid, 0, sizeof(grid));
+  P2G();
+  grid_op();
+  G2P();
 }
 
 int mpm::mesh(const int i,const int j) {
@@ -337,10 +232,6 @@ void mpm::P2G() {
     auto mu = mu_0 * e, lambda = lambda_0 * e;
     if (p.material == Water)  // 流体
       mu = 0;
-    if(p.material == Metal){  // 金属
-      mu = 0;
-      lambda = 0;
-    }
     real J = determinant(p.F);
     Mat r, s;
     polar_decomp(p.F, r, s);
@@ -359,93 +250,57 @@ void mpm::P2G() {
       }
     }
   }
-
-  grid_indices.clear();
-  grid_indices.reserve((n+1) * (n+1));
-  for (int i = 0; i <= n; i++) {
-    for (int j = 0; j <= n; j++) {
-      auto& g = grid[i][j];
-      if (g[2] > 0) {
-        grid_indices.push_back(Vector2i(i, j));
-      }
-    }
-  }
 }
 
 void mpm::grid_op() {
-  for (auto& ind_i : grid_indices) {
-      auto& g = grid[ind_i.x][ind_i.y];
+  for (int i = 0; i <= n; i++) {
+    for (int j = 0; j <= n; j++) {
+      auto& g = grid[i][j];
       // No need for epsilon here
+      if (g[2] > 0) {
         // Normalize by mass
-      g /= g[2];
+        g /= g[2];
+        // Gravity
+        //g += dt * Vector3(0, -200, 0);//gravity
 
-      // Gravity
-      // g += dt * Vector3(0, -200, 0);//gravity
+        //center collision circle
+        for (size_t k = 0; k < obstacles.size(); k++){
+            Vec center = Vec(obstacles[k][0], obstacles[k][1]);
+            real radius = obstacles[k][2];
+            Vec dist = Vec(i, j) * dx - center;
+            real dist2 = sqrt(dist.x * dist.x + dist.y * dist.y);
+            if (dist2 <= radius*dx / DX) {
+                dist = dist / dist2;
+                real n_normal = g[0] * dist.x + g[1] * dist.y;  // dot product
+                n_normal = n_normal < 0 ? 2 * n_normal : 0;
+                g -= Vector3(n_normal * dist.x, n_normal * dist.y, 0);
+            }
+        }
 
-      // center planet      
-      //in the example, there is only one center_planet
-      Vec gravity = Vec(0);
 
-      for (auto& ind_j : grid_indices) {
-        auto& g2 = grid[ind_j.x][ind_j.y];
-        if (ind_i == ind_j)
-          continue;
-        Vec d = (Vec(ind_i.x, ind_i.y) - Vec(ind_j.x, ind_j.y)) * dx;
-        real dist = sqrt(d.x * d.x + d.y * d.y);
-        gravity += -G * g2[2] / (dist * dist * dist) * d;
-      }
-      g += dt * Vector3(gravity.x, gravity.y, 0);
+        // boundary thickness
+        real boundary = 0.05;
+        // Node coordinates
+        real x = (real)i / n;
+        real y = (real)j / n;
 
-      if(center_planet.mass != 0){
-        Vec center = center_planet.center;
-        real r = center_planet.radius * dx / DX;
-        Vec d = Vec(ind_i.x, ind_i.y) * dx - center;
-        real dist = sqrt(d.x * d.x + d.y * d.y);
-        if (dist <= r) {
-          g = Vector3(0, 0, g[2]);
-        } 
-         else {
-           gravity = -G * center_planet.mass / (dist * dist * dist) * d;
-           g += dt * Vector3(gravity.x, gravity.y, 0);
-         }
-      }
-      
-
-      // center collision circle
-      for (size_t k = 0; k < obstacles.size(); k++) {
-        Vec center = Vec(obstacles[k][0], obstacles[k][1]);
-        real radius = obstacles[k][2] * dx / DX;
-        Vec d = Vec(ind_i.x, ind_i.y) * dx - center;
-        real dist = sqrt(d.x * d.x + d.y * d.y);
-        if (dist <= radius) {
-          d = d / dist;
-          real n_normal = g[0] * d.x + g[1] * d.y;  // dot product
-          n_normal = n_normal < 0 ? 2 * n_normal : 0;
-          g -= Vector3(n_normal * d.x, n_normal * d.y, 0);
+        // Sticky boundary
+        
+        if(x < boundary){
+            g[0] = std::max(0.0f, g[0]);
+        }
+        if(x > 1 - boundary){
+            g[0] = std::min(0.0f, g[0]);
+        }
+        // Separate boundary
+        if (y < boundary) {
+          g[1] = std::max(0.0f, g[1]);
+        }
+        if (y > 1 - boundary) {
+          g[1] = std::min(0.0f, g[1]);
         }
       }
-
-      // boundary thickness
-      real boundary = 0.01;
-      // Node coordinates
-      real x = (real)ind_i.x / n;
-      real y = (real)ind_i.y / n;
-
-      // Sticky boundary
-
-      if (x < boundary && g[0]<0) {
-        g[0] = -g[0];
-      }
-      if (x > 1 - boundary && g[0]>0) {
-        g[0] = -g[0];
-      }
-      // Separate boundary
-      if (y < boundary && g[1]<0) {
-        g[1] = -g[1];
-      }
-      if (y > 1 - boundary && g[1]>0) {
-        g[1] = -g[1];
-      }
+    }
   }
 }
 
@@ -476,7 +331,7 @@ void mpm::G2P() {
 
     // Advection
     p.x += dt * p.v;
-  
+
     // MLS-MPM F-update
     auto F = (Mat(1) + dt * p.C) * p.F;
 
@@ -498,85 +353,8 @@ void mpm::G2P() {
     }
     ////////////////////////////////////////////////////////////////////////////////
   }
-  satellite.center = particles[0].x;
 }
 
-
-void mpm::Roche_model() {
-    std::vector<Vec> force;
-    force.resize(particles.size());
-    Vec gravity = Vec(0);
-    Vec radius;
-    real dist;
-    real rho;
-
-    for (int i = 0; i < force.size(); i++) {
-        gravity = Vec(0);
-        radius = particles[i].x - satellite.center;
-        dist = sqrt(radius.x * radius.x + radius.y * radius.y);
-        rho = satellite.rho * particle_mass;
-        if(dist > satellite.radius / window_size){
-          for (int j = 0; j < particles.size(); j++) {
-            if (i == j)
-                continue;
-            Vec r = particles[i].x - particles[j].x;
-            real d = sqrt(r.x * r.x + r.y * r.y);
-            if (d <= 0.0001)
-                continue;
-            gravity += -G * particle_mass / real(pow(d, 3)) * r;
-          }
-        }
-        else
-        {
-            gravity += -G * rho * radius;
-        }
-
-        
-        radius = particles[i].x - center_planet.center;
-        dist = sqrt(radius.x * radius.x + radius.y * radius.y);
-        if (dist > center_planet.radius / window_size) {
-            gravity += -radius * G * center_planet.mass / real((pow(dist, 3)));
-        }
-        force[i] = gravity;
-    }
-    real boundary = 0.01;
-    
-    for (int i = 0; i < particles.size(); i++) {
-        Particle &p = particles[i];
-        if(p.x.x < boundary){
-            p.v.x = max(p.v.x, 0.0_f);        
-        }else if(p.x.x > 1 - boundary){
-            p.v.x = min(p.v.x, 0.0_f);
-        }else if(p.x.y < boundary){
-            p.v.y = max(p.v.y, 0.0_f);
-        }else if(p.x.y > 1 - boundary){
-            p.v.y = min(p.v.y, 0.0_f);
-        }else {
-            Vec radius = p.x - center_planet.center;
-            real dist = sqrt(radius.x * radius.x + radius.y * radius.y);
-            if (dist < center_planet.radius / window_size) {
-                p.v = Vec(0);
-            }else{
-                p.v += dt * force[i];
-                p.x += dt * p.v;
-            }
-        }
-        
-    }
-    //for (auto iter = particles.begin(); iter != particles.end(); iter++) {
-    //    if(check_outrange((*iter).x))
-    //        particles.erase(iter);
-    //}
-    radius = satellite.center - center_planet.center;
-    //satellite.center += satellite.omega * dt * (Vec(-radius.y, radius.x));
-    satellite.center = particles[0].x;
-    orbit.push_back(satellite.center);
-}
-bool mpm::check_outrange(Vec p) {
-    if (p.x < 0 || p.x > 1 || p.y < 0 || p.y > 1)
-        return true;
-    return false;   
-}
 /* -----------------------------------------------------------------------------
 ** Reference: A Moving Least Squares Material Point Method with Displacement
               Discontinuity and Two-Way Rigid Body Coupling (SIGGRAPH 2018)
@@ -656,5 +434,4 @@ Questions go to yuanming _at_ mit.edu
                                                       Version 1.5
 
 ----------------------------------------------------------------------------- */
-
 
