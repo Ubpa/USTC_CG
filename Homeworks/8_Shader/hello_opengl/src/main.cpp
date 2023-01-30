117c7161e15f26f3a66e5bf4b4ea7b2b2f0e3908
Rainzor <runze.wang02@outlook.com>
2023-01-30 10:49:21 +0800

    modified

--- Homeworks/8_Shader/hello_opengl/src/main.cpp
+++ Homeworks/8_Shader/hello_opengl/src/main.cpp
@@ -1,8 +1,8 @@
-#define GLFW_INCLUDE_NONE
-
-#include <GLFW/glfw3.h>
 #include <glad/glad.h>
 
+#include <GLFW/glfw3.h>
+
+#include "shader.h"
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -11,59 +11,115 @@ void processInput(GLFWwindow* window);
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-
 int main() {
-    // glfw: initialize and configure
-    // ------------------------------
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
+     // glfw: initialize and configure
+     // ------------------------------
+     glfwInit();
+     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // glfw window creation
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    if (window == NULL) {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+     // glfw window creation
+     // --------------------
+     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+     if (window == NULL) {
+         std::cout << "Failed to create GLFW window" << std::endl;
+         glfwTerminate();
+         return -1;
+     }
+     glfwMakeContextCurrent(window);
+     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+     // glad: load all OpenGL function pointers
+     // ---------------------------------------
+     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+         std::cout << "Failed to initialize GLAD" << std::endl;
+         return -1;
+     }
 
-    // render loop
-    // -----------
-    while (!glfwWindowShouldClose(window)) {
-        // input
-        // -----
-        processInput(window);
+     // build and compile our shader program
+     // ------------------------------------
+     // main.cpp
+     Shader ourShader("..\\shaders\\shader.vs", "..\\shaders\\shader.fs");  // you can name your shader files however you like
 
-        // render
-        // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+     // set up vertex data (and buffer(s)) and configure vertex attributes
+     // ------------------------------------------------------------------
+     // 更多属性 位置+颜色
+     float vertices[] = {
+         // positions         // colors
+         0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,   // bottom right
+         -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom left
+         0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f     // top
 
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+     };
 
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
-    glfwTerminate();
-    return 0;
+     unsigned int VBO, VAO;
+     glGenVertexArrays(1, &VAO);
+     glGenBuffers(1, &VBO);
+     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+     glBindVertexArray(VAO);
+
+     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+     // 设置顶点属性指针，告诉OpenGL如何解析顶点数据
+     // 参数1：顶点属性的位置值，对应顶点着色器中layout(location = 0)的位置值
+     // 参数2：顶点属性的大小，vec3是3个值
+     // 参数3：顶点属性的类型
+     // 参数4：是否希望数据被标准化，如果设置为GL_TRUE，所有数据都会被映射到0（对于有符号型signed数据是-1）到1之间
+     // 参数5：步长，连续顶点属性组之间的间隔
+     // 参数6：偏移量，位置数据在缓冲中起始位置的偏移量
+     // position attribute
+     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+     glEnableVertexAttribArray(0);
+     // color attribute
+     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+     glEnableVertexAttribArray(1);
+
+     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+     // glBindVertexArray(0);
+
+     // as we only have a single shader, we could also just activate our shader once beforehand if we want to
+     ourShader.use();
+
+     // render loop
+     // -----------
+     while (!glfwWindowShouldClose(window)) {
+         // input
+         // -----
+         processInput(window);
+
+         // render
+         // ------
+         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+         glClear(GL_COLOR_BUFFER_BIT);
+
+         // shader
+         // ------
+         ourShader.use();
+
+         // render the triangle
+         glBindVertexArray(VAO);//绑定VAO
+         glDrawArrays(GL_TRIANGLES, 0, 3);
+
+         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+         // -------------------------------------------------------------------------------
+         glfwSwapBuffers(window);
+         glfwPollEvents();
+     }
+
+     // optional: de-allocate all resources once they've outlived their purpose:
+     // ------------------------------------------------------------------------
+     glDeleteVertexArrays(1, &VAO);
+     glDeleteBuffers(1, &VBO);
+     ourShader.deleteShader();
+
+     // glfw: terminate, clearing all previously allocated GLFW resources.
+     // ------------------------------------------------------------------
+     glfwTerminate();
+     return 0;
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
---
+++ Homeworks/8_Shader/hello_opengl/src/shader.cpp
@@ -0,0 +1,110 @@
+#include "shader.h"
+
+Shader::Shader(const char* vertexPath, const char* fragmentPath) {
+    // 1. 从文件路径中获取顶点/片段着色器
+    std::string vertexCode;
+    std::string fragmentCode;
+    std::ifstream vShaderFile;
+    std::ifstream fShaderFile;
+    // 保证ifstream对象可以抛出异常：
+    vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try {
+        // 打开文件
+        vShaderFile.open(vertexPath);
+        fShaderFile.open(fragmentPath);
+        std::stringstream vShaderStream, fShaderStream;
+        // 读取文件的缓冲内容到数据流中
+        vShaderStream << vShaderFile.rdbuf();
+        fShaderStream << fShaderFile.rdbuf();
+        // 关闭文件处理器
+        vShaderFile.close();
+        fShaderFile.close();
+        // 转换数据流到string
+        vertexCode = vShaderStream.str();
+        fragmentCode = fShaderStream.str();
+    } catch (std::ifstream::failure e) {
+        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+    }
+    const char* vShaderCode = vertexCode.c_str();
+    const char* fShaderCode = fragmentCode.c_str();
+
+    // 2. 编译着色器
+    unsigned int vertex, fragment;
+
+    // 顶点着色器
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vShaderCode, NULL);
+    glCompileShader(vertex);
+    // 打印编译错误（如果有的话）
+    checkCompileErrors(vertex, "VERTEX");
+
+    // 片段着色器
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fShaderCode, NULL);
+    glCompileShader(fragment);
+    checkCompileErrors(fragment, "FRAGMENT");
+
+    // 着色器程序
+    ID = glCreateProgram();
+    glAttachShader(ID, vertex);
+    glAttachShader(ID, fragment);
+    glLinkProgram(ID); 
+    // 打印连接错误（如果有的话）
+    checkCompileErrors(ID, "PROGRAM");
+
+    // 删除着色器，它们已经链接到我们的程序中了，已经不再需要了
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+}
+
+Shader::Shader(const std::string vertexPath, const std::string fragmentPath){
+    Shader(vertexPath.c_str(), fragmentPath.c_str());
+}
+
+void Shader::use() {
+    glUseProgram(ID);
+}
+
+void Shader::deleteShader() {
+    glDeleteProgram(ID);
+}
+
+//设置Uniform
+//参数1：uniform变量名
+//参数2：uniform变量值
+//利用glGetUniformLocation函数获取uniform变量的位置(提供变量名和着色器程序ID)
+//利用glUniform**函数设置uniform变量的值
+void Shader::setBool(const std::string& name, bool value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+
+void Shader::setInt(const std::string& name, int value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setFloat(const std::string& name, float value) const {
+    glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::checkCompileErrors(unsigned int shader, std::string type) {
+    int success;
+    char infoLog[1024];
+    if (type != "PROGRAM") {
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+            std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n"
+                      << infoLog << std::endl;
+            std::cout << " ----------------------------------------------------- -- " << std::endl;
+        }else{
+            glGetProgramiv(shader, GL_LINK_STATUS, &success);
+            if (!success) {
+                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+                std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n"
+                          << infoLog << std::endl;
+                std::cout << " ----------------------------------------------------- -- " << std::endl;
+            }
+        }
+    }
+}
